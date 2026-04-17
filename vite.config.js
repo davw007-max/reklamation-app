a46c412044cc9e16deb898831ef984c42571f7fe
David Wallis <davw007@googlemail.com>
2026-04-17 07:20:53 +0200

    remove pwa config

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: [],
-      manifest: {
-        name: "Auftrags App",
-        short_name: "Aufträge",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#000000",
-        theme_color: "#000000",
-        icons: [
-          {
-            src: "icon-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "icon-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
-});
+  plugins: [react()],
+})
